71d8c0dffe44fbff60c7b09d461849ba74b0ac77
ehandresen <eirik.horgen.andresen@gmail.com>
2024-04-16 21:25:21 +0200

    __str__ method in classes

--- 01/classes.py
+++ 01/classes.py
@@ -3,5 +3,14 @@ class Person:
         self.name = name
         self.age = age
 
+    def __str__(self):
+        return f'Name: {self.name}, Age: {self.age}'
+
+    def introduce(self):
+        print(f'Hello, my name is {self.name} and I am {self.age} years old')
+
 p1 = Person('Eirik', 29)
-print(type(p1))
+p2 = Person('Jack', 25)
+
+print(p1)
+print(p2)
