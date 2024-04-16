dcb6f4dde2d444c189497a594d779b28a81d6560
ehandresen <eirik.horgen.andresen@gmail.com>
2024-04-16 21:51:25 +0200

    simple getters & setters

--- 01/classes.py
+++ 01/classes.py
@@ -2,15 +2,28 @@ class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
+    
+    def get_age(self):
+        return self.age
+    
+    def set_age(self, new_age):
+        self.age = new_age
+    
+    def get_name(self):
+        return self.name
+    
+    def set_name(self, new_name):
+        self.name = new_name
 
     def __str__(self):
         return f'Name: {self.name}, Age: {self.age}'
 
-    def introduce(self):
-        print(f'Hello, my name is {self.name} and I am {self.age} years old')
+    
 
 p1 = Person('Eirik', 29)
-p2 = Person('Jack', 25)
+
+
+p1.set_name('jallaballa')
+name = p1.get_name()
 
 print(p1)
-print(p2)
